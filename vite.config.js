682ce39e2d116a78b268ca2fe312b3b5eaa43efb
Marilyn Yu <marilynyu@berkeley.edu>
2025-05-13 15:34:07 -0500

    changed vite

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,5 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/', // <- This must match your repo name
-  plugins: [react()]
+  plugins: [react()],
 })
